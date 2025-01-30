aeb67f901a9df3bc0541d0a5da8fd5f8b46ab412
saahen-sriyan-mishra <139043263+saahen-sriyan-mishra@users.noreply.github.com>
2025-01-31 00:47:30 +0530

    All done

--- src/Components/Sample/Sample.jsx
+++ src/Components/Sample/Sample.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Sample.scss';
-import img1 from ''
+import img1 from './sample_images/Blog.png';
+import img2 from './sample_images/Case Study.png';
+import img3 from './sample_images/Copywriting.png';
+import img4 from './sample_images/Newsletter.png';
+import img5 from './sample_images/Social media.png';
+import img6 from './sample_images/Website.png';
+import img7 from './sample_images/Service.png';
 import Footer from '../Home/Footer';
 
 const Sample = () => {
@@ -11,7 +17,7 @@ const Sample = () => {
           <a href="https://drive.google.com/file/d/1rUgwTmqn2Oshu3EfOma92YpKepGwEcm7/view?usp=sharing " target="_blank"><div className="grid-item1">
             <span>Service Deck</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img7} alt="" />
             </div>
           </div></a>
 
@@ -25,7 +31,7 @@ const Sample = () => {
           <a href="https://drive.google.com/file/d/1YLsJCLaoFXxooxL8aQ2QmVNGXBOpsQox/view?usp=sharing " target="_blank"><div className="grid-item3">
             <span>Newsletter</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img4} alt="" />
             </div>
           </div></a>
 
@@ -36,28 +42,28 @@ const Sample = () => {
           <a href="https://drive.google.com/file/d/1H5ROCiJwGnNyqRrTR4770C5At8jlfznY/view?usp=sharing " target="_blank"><div className="grid-item5">
             <span>Copywriting</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img3} alt="" />
             </div>
           </div></a>
 
           <a href="https://drive.google.com/file/d/15wZhYaKnXhknvcuqeLjn2OU-OktDOTxr/view?usp=sharing" target="_blank"><div className="grid-item6">
             <span>Case Study</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img2} alt="" />
             </div>
           </div></a>
 
           <a href="https://drive.google.com/file/d/1YrJTV4v3tQuYjg3L6wP7lKVJp0y98X3R/view?usp=drive_link" target="_blank"><div className="grid-item7">
             <span>Website Content</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img6} alt="" />
             </div>
           </div></a>
 
           <a href="https://drive.google.com/file/d/1uBPSxkOSEPMN8Ahq0LOpbqBl4byKt2ww/view?usp=drive_link" target="_blank"><div className="grid-item8">
             <span>Social Media</span>
             <div className="image">
-              <img src={img1} alt="" />
+              <img src={img5} alt="" />
             </div>
           </div></a>
         </div>
